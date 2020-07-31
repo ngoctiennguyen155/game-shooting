92b081d9fd248726f77e40b3f230737ec939162c
TienNguyen <ngoctiennguyen155@gmail.com>
2020-07-31 22:54:43 +0700

    +first ideagameshooting

---
+++ Assets/Scripts/Shooting.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+
+    public float bullerForce = 1000;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyUp(KeyCode.Space))
+        {
+            Shoot();
+        }
+    }
+    void Shoot()
+    {
+        Vector3 huongdan = new Vector3(firePoint.position.x,firePoint.position.y,firePoint.position.z);
+        GameObject bullet = Instantiate(bulletPrefab,huongdan,firePoint.rotation);
+        Rigidbody rbbullet = bullet.GetComponent<Rigidbody>();
+        rbbullet.AddForce(-firePoint.forward * bullerForce,ForceMode.Impulse);
+    }
+}
---
+++ Assets/addforce.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class addforce : MonoBehaviour
+{
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.AddForce(Vector3.up);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
