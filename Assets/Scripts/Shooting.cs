b4d871558f354566ec521997e31edfe1870c39ee
TienNguyen <ngoctiennguyen155@gmail.com>
2020-08-01 21:14:04 +0700

    +setting lighting

--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -4,25 +4,25 @@ using UnityEngine;
 
 public class Shooting : MonoBehaviour
 {
-    // Start is called before the first frame update
-    public Transform firePoint;
-    public GameObject bulletPrefab;
+     // Start is called before the first frame update
+     public Transform firePoint;
+     public GameObject bulletPrefab;
 
-    public float bullerForce = 1000;
+     public float bullerForce = 1000;
 
-    // Update is called once per frame
-    void Update()
-    {
-        if(Input.GetKeyUp(KeyCode.Space))
-        {
-            Shoot();
-        }
-    }
-    void Shoot()
-    {
-        Vector3 huongdan = new Vector3(firePoint.position.x,firePoint.position.y,firePoint.position.z);
-        GameObject bullet = Instantiate(bulletPrefab,huongdan,firePoint.rotation);
-        Rigidbody rbbullet = bullet.GetComponent<Rigidbody>();
-        rbbullet.AddForce(-firePoint.forward * bullerForce,ForceMode.Impulse);
-    }
+     // Update is called once per frame
+     void Update()
+     {
+          if (Input.GetKeyUp(KeyCode.Mouse0))
+          {
+               Shoot();
+          }
+     }
+     void Shoot()
+     {
+          Vector3 huongdan = new Vector3(firePoint.position.x, firePoint.position.y, firePoint.position.z);
+          GameObject bullet = Instantiate(bulletPrefab, huongdan, firePoint.rotation);
+          Rigidbody rbbullet = bullet.GetComponent<Rigidbody>();
+          rbbullet.AddForce(-firePoint.forward * bullerForce, ForceMode.Impulse);
+     }
 }
